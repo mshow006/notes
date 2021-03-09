a629ba2c678d38c85ed6571aedfc4c1fb4fe8501
yingming006 <yingming006@foxmail.com>
2021-03-09 12:58:26 +0800

    update

--- network/tcpip-network/chapter06/bound_host2.c
+++ network/tcpip-network/chapter06/bound_host2.c
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
     char msg2[] = "I'm another UDP host";
     char msg3[] = "Nice to meet you";
     struct sockaddr_in your_adr;
-    socklen_t adr_sz;
+    socklen_t your_adr_sz;
 
     if (argc != 3)
     {
@@ -30,8 +30,8 @@ int main(int argc, char *argv[])
 
     memset(&your_adr, 0, sizeof(your_adr));
     your_adr.sin_family = AF_INET;
-    your_adr.sin_addr.s_addr = htonl(INADDR_ANY);
-    your_adr.sin_port = htons(atoi(argv[1]));
+    your_adr.sin_addr.s_addr = inet_addr(argv[1]);
+    your_adr.sin_port = htons(atoi(argv[2]));
 
     sendto(sock, msg1, sizeof(msg1), 0, (struct sockaddr *)&your_adr, sizeof(your_adr));
     sendto(sock, msg2, sizeof(msg2), 0, (struct sockaddr *)&your_adr, sizeof(your_adr));
